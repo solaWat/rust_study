377d4377648a58e54515e9bf9230773ad68e096d
solawat <s.spthx.555@gmail.com>
2022-11-16 22:32:03 +0900

    finish

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
+    // println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
 
     loop {
         println!("Please input your guess.");
@@ -18,14 +18,19 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!"); //数値を入力してください！
-
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),  //小さすぎ！
             Ordering::Greater => println!("Too big!"), //大きすぎ！
-            Ordering::Equal => println!("You win!"),   //やったね！
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
