8ab4fd9eb7999b71e20d6293989bc7c33b423214
solawat <s.spthx.555@gmail.com>
2022-11-24 22:29:17 +0900

    [temp]別ファイルにしてmoduleとして呼び出す

--- projects/metrics/src/main.rs
+++ projects/metrics/src/main.rs
@@ -1,30 +1,5 @@
-use std::collections::HashMap;
+mod metrics;
 
-use actix_web::{web, App, HttpResponse, HttpServer};
-use actix_web_prom::{PrometheusMetrics, PrometheusMetricsBuilder};
-
-async fn health() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let mut labels = HashMap::new();
-    labels.insert("application".to_string(), "kfcore".to_string());
-    let prometheus = PrometheusMetricsBuilder::new("api")
-        .endpoint("/metrics")
-        .const_labels(labels)
-        .build()
-        .unwrap();
-
-        HttpServer::new(move || {
-            App::new()
-                .wrap(prometheus.clone())
-                .service(web::resource("/health").to(health))
-        })
-        // curl -i localhost:8080/health
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await?;
-    Ok(())
+fn main() {
+    metrics::main();
 }
